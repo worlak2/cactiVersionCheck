c3709bcc34cd053534ab21ac4fdb1fbdaa169829
worlak2 <worlak2@mail.ru>
2017-10-25 20:12:20 +0300

    New

--- check.py
+++ check.py
@@ -1,9 +1,31 @@
 import requests
 import sys
-
+import argparse
 from bs4 import BeautifulSoup
+__author__ = 'worlak2 & Cibvetr'
+__version__ = '0.1'
 
-yourHost = str(input("enter host without http\n"))
+parser = argparse.ArgumentParser(
+    description='Command-line tool for Cacti version check')
+parser.add_argument('-H', '--host', help='Host to check', type=str)
+parser.add_argument(
+    '-p',
+    '--port',
+    help='Port on which Cacti is located',
+    type=str)
+
+args = parser.parse_args()
+
+if args.host is None:
+    print('See help -h --help')
+    sys.exit()
+else:
+    yourHost = args.host
+if args.port is None:
+    port = '80'
+else:
+    port = args.port
+
 url = 'https://www.cacti.net/'
 r = requests.get(url)
 versionText = 'version='
@@ -14,17 +36,19 @@ url = 'https://www.cacti.net/changelog.php'
 r = requests.get(url)
 
 
-def get_all_vrsion(html):
+def get_all_version(html):
     soup = BeautifulSoup(html, 'lxml')
     version = soup.find_all('td', class_='textParagraphHeading')
     return version
 
 
-if (requests.get("http://" + yourHost + "/cacti/install/").status_code == 200):
-    yourHost = yourHost + "/cacti/install/"
+if requests.get('http://' + yourHost + ':' + port +
+                '/cacti/install/').status_code == 200:
+    yourHost = yourHost + ':' + port + '/cacti/install/'
 else:
-    yourHost = yourHost + "/install/"
-ark = get_all_vrsion(r.text)
+    yourHost = yourHost + ':' + port + '/install/'
+
+ark = get_all_version(r.text)
 textt = ''
 textt += str(ark)
 new = textt.replace('</td>, <td class="textParagraphHeading">', '')
@@ -37,26 +61,28 @@ final.remove('')
 i = 0
 d = 0
 final.reverse()
-cactiversion = ""
+cactiversion = ''
 
 while i < final.__len__():
     sys.stderr.write('%d\r' % i)
     if final[i] == finalVersion:
         break
-    if final[i] == "0.8.8":
+    if final[i] == '0.8.8':
         while d != 8:
-            sendtourl = "0_8_8" + "_" + "to_" + final[i + 1].replace(".", "_") + ".php"
-            r = requests.get("http://" + yourHost + sendtourl.replace(" ", ""))
+            sendtourl = '0_8_8' + '_' + 'to_' + \
+                final[i + 1].replace('.', '_') + '.php'
+            r = requests.get('http://' + yourHost + sendtourl.replace(' ', ''))
             if r.status_code == 200:
-                cactiversion = sendtourl.replace(" ", "")
+                cactiversion = sendtourl.replace(' ', '')
             d += 1
             i += 1
-    sendtourl = final[i].replace(".", "_") + "_" + "to_" + final[i + 1].replace(".", "_") + ".php"
-    r = requests.get("http://" + yourHost + sendtourl.replace(" ", ""))
+    sendtourl = final[i].replace('.', '_') + '_' + \
+        'to_' + final[i + 1].replace('.', '_') + '.php'
+    r = requests.get('http://' + yourHost + sendtourl.replace(' ', ''))
     if r.status_code == 200:
-        cactiversion = sendtourl.replace(" ", "")
+        cactiversion = sendtourl.replace(' ', '')
     i += 1
 
-totext = cactiversion.find("to_")
+totext = cactiversion.find('to_')
 print(cactiversion)
-print("your version\n" + cactiversion[totext + 3:-4].replace("_", "."))
+print('your version\n' + cactiversion[totext + 3:-4].replace('_', '.'))
