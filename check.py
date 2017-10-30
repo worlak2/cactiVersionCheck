7047caf4bf9c4007b6ad9d9fd2df01bcfee35125
worlak2 <worlak2@mail.ru>
2017-10-30 14:48:33 +0300

    Fix work on Mac OS

--- check.py
+++ check.py
@@ -1,6 +1,8 @@
 import requests
 import sys
 import argparse
+from tqdm import tqdm
+requests.packages.urllib3.disable_warnings()
 from bs4 import BeautifulSoup
 __author__ = 'worlak2 & Cibvetr'
 __version__ = '0.1'
@@ -9,11 +11,10 @@ parser = argparse.ArgumentParser(
     description='Command-line tool for Cacti version check')
 parser.add_argument('-H', '--host', help='Host to check', type=str)
 parser.add_argument(
-    '-P',
+    '-p',
     '--port',
     help='Port on which Cacti is located',
     type=str)
-
 args = parser.parse_args()
 
 if args.host is None:
@@ -27,18 +28,20 @@ else:
     port = args.port
 
 url = 'https://www.cacti.net/'
-r = requests.get(url)
+r = requests.get(url,verify=False)
 versionText = 'version='
 start = r.text.find(versionText)
 end = r.text.find('"', start)
 finalVersion = r.text[start + len(versionText):end]
 url = 'https://www.cacti.net/changelog.php'
-r = requests.get(url)
+r = requests.get(url,verify=False)
+
 
 def get_all_version(html):
     soup = BeautifulSoup(html, 'lxml')
     version = soup.find_all('td', class_='textParagraphHeading')
     return version
+
 
 if requests.get('http://' + yourHost + ':' + port +
                 '/cacti/install/').status_code == 200:
@@ -61,8 +64,8 @@ d = 0
 final.reverse()
 cactiversion = ''
 
-while i < final.__len__():
-    sys.stderr.write('%d\r' % i)
+for i in tqdm(range(final.__len__())):
+
     if final[i] == finalVersion:
         break
     if final[i] == '0.8.8':
